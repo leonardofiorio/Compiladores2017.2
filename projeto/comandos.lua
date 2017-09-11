8517980a0166a469e9e87f833cd8cab20d5c8b88
Fernando Da Rós Filho <fdarosfilho@gmail.com>
2017-09-11 08:44:18 -0300

    While exp

--- projeto/comandos.lua
+++ projeto/comandos.lua
@@ -70,25 +70,65 @@ end
 
 function tratamentoComandos(s, m, c)
   -- Para tratamento de while
-  tmp = s:pop(1)
+  tmp = c:pop(1)
 
   if tmp == "while" then
-
+    cond = pegarCondicao(s, m, c)
+    if cond ~= nil then
+      print("ok")
+      for _, x in ipairs(cond) do
+        print(x)
+      end
+      --considerando que primeiro venha a variavel e depois a contatante a < 10
+      local a = m[cond[1]]
+      local simbolo = cond[2]
+      local constante = cond[3]
+      if simbolo == "==" then
+      elseif simbolo == "~=" then
+      elseif simbolo == "<" then
+        while a < tonumber(constante) do
+          print(a)
+          m[cond[1]] = m[cond[1]] + 1
+          a = m[cond[1]]
+        end
+      elseif simbolo == "<=" then
+      elseif simbolo == ">" then
+      elseif simbolo == ">=" then                
+      end
+    else
+      print("Condição inválida.")
+    end
   elseif tmp == "" then
-
+    print('end');
   end
+print(tmp)
 
+end
+
+function pegarCondicao(s, m, c)
+  cond = {}
+  local value = c:pop(1)
+  while value ~= "do" do
+    if value == nil then
+      return nil
+    end
+    table.insert(cond, value)
+    value = c:pop(1)
+  end
+  return cond
 end
 
  
 -- -------------------------------- SMC ---------------------------------------
 
 s = Stack:Create()
-m = Stack:Create()
+--m = Stack:Create()
+m = {}
+m['a'] = -1
 c = Stack:Create()
 
 
-entrada = {"while", "a < 10", "do", "print(a)", "end"}
+entrada = {"while", "a", "<", "10", "do", "print(a)", "a", "=", "a", "+", "1", "end"}
 
 
 tamanho_entrada = table.maxn(entrada)
