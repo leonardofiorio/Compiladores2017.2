55cea477e27280988ec2860b849f8eb8579063a6
Leonardo <leonardofiorio@id.uff.br>
2017-09-13 00:01:58 -0300

    Iniciando organização e tratamento de entrada

--- projeto/input_smc.lua
+++ projeto/input_smc.lua
@@ -1,7 +1,21 @@
-entrada = "exp(sub(sub(5,1),2))"
+--require "expressoes"
+--require "comandos"
 
 function input(e)
-	tmp = string(e, 0, 2)
-	print(tmp)
+	tmp = string.sub(e, 0, 3)
+	if tmp == "exp" then
+		print("Expressões")
+		-- Fazer leitura da expressão
+		input()
+	elseif tmp == "cmd" then
+		print("Comandos")
+	elseif tmp == "add" then
 
+	elseif tmp == "sub" then
+		
+	end
 end
+
+entrada = "cmd(sub(sub(5,1),2))"
+input(entrada)
+
