fcb9cc7a59ce0402370f13b57ddae23453e785cf
Fernando Da Rós Filho <fdarosfilho@gmail.com>
2017-10-11 07:32:13 -0300

    Rework parser

---
+++ projeto/parser/bin/luafish.lua
@@ -0,0 +1,54 @@
+-- luafish.lua
+-- This is a front-end to the LuaFish macro processor.
+--
+-- Example usage:
+--    export LUA_PATH='lib/?.lua;examples/?.lua;?.lua'
+--    lua bin/luafish.lua examples/type_usage2.lua
+
+function printAST(ast)
+  print("\n\n")
+  for key,value in ipairs(ast) do --each ';' determines 1 key
+    print(key)
+    print(value)
+  end
+end
+
+-- function printAST(node, level)
+--     if level == nil then
+--         level = 0
+--     end
+--     if node ~= nil then
+--         print(string.rep(" ", level) .. "Node[" .. node.data .. "]")
+--         if(node.children ~= nil) then
+--           for _, child in ipairs(node.children) do
+--             Tree.show(child, level + 2)
+--           end
+--         end
+--     end
+-- end
+
+local macro = require 'luafish.macro'
+
+macro.addloader()
+
+local is_file = false
+local start = 1
+while true do
+  local v = select(start, ...)
+  if not(v and v:find '-' == 1 and #v > 1) then
+    break
+  end
+  if v == '-file' then
+    is_file = true
+  else
+    error('unrecognized option ' .. v)
+  end
+  start = start + 1
+end
+local code = select(start, ...)
+
+local Parser = require 'luafish.parser'
+local p = Parser()
+print(p:parse{code, is_file})
+printAST(p:parse{code, is_file})
+
